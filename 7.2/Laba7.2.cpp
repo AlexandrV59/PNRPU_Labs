818eeacdc107bc416d4513c7e644d62e3460e8db
AlexandrV59 <kent1234810@gmail.com>
2021-02-11 23:44:07 +0500

    little fix

---
+++ 7.2/Laba7.2.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+void pass()
+{
+    cout<<"Hello vim"<<endl;
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    pass();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
