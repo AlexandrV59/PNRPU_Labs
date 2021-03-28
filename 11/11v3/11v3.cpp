9a7256235dd447c9567f39f07d2df68bd5b4b7e5
AlexandrViktorovich <kent1234810@gmail.com>
2021-03-28 16:49:26 +0500

    Report added

--- 11/11v3/11v3.cpp
+++ 11/11v3/11v3.cpp
@@ -15,16 +15,16 @@ return NULL;
 stack* top, *p;
 top = NULL;
 p = new stack;
-cout « "Enter first element";
-cin » a;
+cout << "Enter first element";
+cin >> a;
 p->data = a;
 p->prev=NULL;
 top = p;
 for (int i = 2; i <= n; i++)
 {
 stack *h = new stack;
-cout « "Enter " « i « "element: ";
-cin » a;
+cout << "Enter " << i << "element: ";
+cin >> a;
 h->data = a;
 h->prev = top;
 top = h;
@@ -36,16 +36,16 @@ void print(stack*top)
 {
 if (top==NULL)
 {
-cout « "Empty stack" « endl;
+cout << "Empty stack" << endl;
 }
 else {
 stack* p = top;
 while(p != NULL)
 {
-cout « p->data « " ";
+cout << p->data << " ";
 p = p->prev;
 }
-cout « endl;
+cout << endl;
 }
 }
 
@@ -53,14 +53,14 @@ void add(stack* &first, int n)
 {
 stack* p = first;
 stack* h = new stack;
-cout « "Enter element: ";
-cin » h->data;
+cout << "Enter element: ";
+cin >> h->data;
 h->prev=p;
 first=h;
 while(p->prev!=NULL){
 stack* h = new stack;
-cout « "Enter elemet to add: ";
-cin » h->data;
+cout << "Enter elemet to add: ";
+cin >> h->data;
 h->prev = p->prev;
 p->prev = h;
 if(p->prev->prev!=NULL)
@@ -75,15 +75,14 @@ p=p->prev;
 
 int main()
 {
-cout«"Stack size: ";
+cout<<"Stack size: ";
 int n;
-cin » n;
-cout « "Enter stack element:" « endl;
+cin >> n;
 stack* st = make_stack(n);
-cout « "Stack:";
+cout << "Stack:";
 print(st);
 add(st, n);
-cout « "After:";
+cout << "After:";
 print(st);
 return 0;
 }
